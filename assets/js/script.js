b3c10379a32bc7afa1875c01abae2b26f300afbe
madyark <kozhakhmetovmadyar@gmail.com>
2021-01-10 11:29:24 +0000

    add javascript code that calls the spoonacular API in the script.js file and fix margins for the user-buttons in the style.css file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,9 @@
 // Picture Game section
 
-function loadFoodItems() { // Loads the initial items in a random order
-    let foodTypeItems = document.getElementsByClassName("food-type-items");
-
-    let foodTypeItemsLength = foodTypeItems.length;
-
-    let newFoodTypeItems = [];
-
-    let i = 0;
-
-    while (i < foodTypeItemsLength) {
-        let mealTypes = ["Burger", "Pizza", "Cake", "Chicken", "Steak", "Kebab", "Sushi", "Burrito", "Sea Food", "Salad", "Sandwich", "Tacos", "Pasta", "Ice Cream", "French Fries", "Pancakes", "Eggs", "Soup",];
-
-        let specificMealType = mealTypes[Math.floor(Math.random() * mealTypes.length)]; // Chooses a random index number between 0 and 17 (length of the mealTypes array minus one) and assigns a random element to the variable
-            // Solution found on a StackOverflow post (https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array)
-
-        if (newFoodTypeItems.includes(specificMealType)) {
-            continue;
-        } else {
-            newFoodTypeItems.push(specificMealType);                
-            foodTypeItems[i].innerHTML = `<span onclick="foodItemSelection()">${newFoodTypeItems[i]}<i class="fa fa-angle-right"></i></span>`; // Each element in the newFoodTypeItems array is added onto the DOM
-            i++;
-        };
-    };
-}
-
-function foodItemSelection() { // Loads the API after one of the food items is selected
+function foodItemSelection(selectedText) { // Loads the API after one of the food items is selected
     let changedBody = document.getElementById("changed-body");
 
+    // Loading of the new HTML code
     changedBody.innerHTML = `
         <h2>Click on one of the images below</h2>
 
@@ -57,13 +33,12 @@ function foodItemSelection() { // Loads the API after one of the food items is s
         </div>
     `    
 
+    // Loading of the images that will change based on clicks
     let imgLeft = document.getElementById("image-left");
     let imgRight = document.getElementById("image-right");
 
     let imgOne = document.querySelector("#image-left img");
     let imgTwo = document.querySelector("#image-right img");
-
-    let clicks = 0;
 
     imgOne.onclick = function() { // This image will appear while the webpage loads an image from the API
         imgLeft.innerHTML = `
@@ -76,4 +51,46 @@ function foodItemSelection() { // Loads the API after one of the food items is s
             <img src="assets/images/loading.gif" alt="Loading image">
             <p>Please wait. Your image is being loaded.</p>`;
     };
+
+    // Calling the Spoonacular API
+    selectedText = selectedText.toLowerCase(); // The selectedText paramater should be lowercase as it will be used as the search query for the API
+    const apiKey = "0bd6828e250e4d07b2474b71ff63b815"; // Unique API key assigned by Spoonacular
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://api.spoonacular.com/food/menuItems/search?${apiKey}&query=${selectedText}`)
+
+    xhr.onreadystatechange = function() { // Setting up the XHR state listener
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
 };
+
+function loadFoodItems() { // Loads the initial items in a random order
+    let foodTypeItems = document.getElementsByClassName("food-type-items");
+
+    let foodTypeItemsLength = foodTypeItems.length;
+
+    let newFoodTypeItems = [];
+
+    let i = 0;
+
+    while (i < foodTypeItemsLength) { // A while loop that allows us to add elements onto the newFoodTypeItems array and print them into the console as long as a certain condition is being met
+        let mealTypes = ["Burger", "Pizza", "Cake", "Chicken", "Steak", "Kebab", "Sushi", "Burrito", "Sea Food", "Salad", "Sandwich", "Tacos", "Pasta", "Ice Cream", "French Fries", "Pancakes", "Eggs", "Soup",];
+
+        let specificMealType = mealTypes[Math.floor(Math.random() * mealTypes.length)]; // Chooses a random index number between 0 and 17 (length of the mealTypes array minus one) and assigns a random element to the variable
+            // Solution found on a StackOverflow post (https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array)
+
+        if (newFoodTypeItems.includes(specificMealType)) {
+            continue; // If the selected element from the mealTypes array is already in the newFoodTypeItems array, the loop will restart 
+        } else {
+            newFoodTypeItems.push(specificMealType);                
+            foodTypeItems[i].innerHTML = `<span onclick="foodItemSelection()">${newFoodTypeItems[i]}<i class="fa fa-angle-right"></i></span>`; // Each element in the newFoodTypeItems array is added onto the DOM
+            foodTypeItems[i].addEventListener("click", function() {
+                let selectedFoodTypeItem = foodTypeItems[i];
+                foodItemSelection(selectedFoodTypeItem); // Creating an event listener for each element in the newFoodTypeItems array that will store the selected food type in a variable and pass it on as a paramater to an outside function
+            });
+            i++;
+        };
+    };
+}
